f38114adc51d7c6c0df52270719b7e27cd113e30
reportik <79289765+reportik@users.noreply.github.com>
2025-08-25 13:59:45 -0600

    get_active_sellable_products: variant_values y display_name

--- main.py
+++ main.py
@@ -662,6 +662,7 @@ async def get_active_sellable_products():
                 "id": prod["id"],
                 "name": display_name,
                 "price": prod["list_price"],
+                "list_price": prod["list_price"],
                 "categ_id": prod["categ_id"],
                 "variant_values": variant_value_ids
             })
